36b84ec24016b87b3ab1ed7cba545f628a0075e0
Pottassiuw <pottassiu@proton.me>
2026-07-20 17:16:00 -0300

    feat(relatorios): aba Mensalizacao (grafico + tabela mensal)

---
+++ frontend/src/features/relatorios/aba-mensalizacao.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { MensalizacaoChart } from './mensalizacao-chart';
+import { TabelaMensal } from './tabela-mensal';
+import type { DashboardRelatorios } from './types';
+
+export function AbaMensalizacao({ data }: { data: DashboardRelatorios }): React.JSX.Element {
+  return (
+    <div className="flex flex-col gap-[16px]">
+      <MensalizacaoChart meses={data.mensalizacao} mesCorrente={data.mes_corrente} />
+      <TabelaMensal meses={data.mensalizacao} mesCorrente={data.mes_corrente} />
+    </div>
+  );
+}
